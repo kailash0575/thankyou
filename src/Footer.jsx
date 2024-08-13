849b9b8236d73a0f45c1cb80d958044b6e1f53b9
Kailash Tanwar <kailash.tanwar@acedigitalsolution.com>
2024-08-13 19:45:07 +0530

    commit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -11,89 +11,89 @@ const Footer = () => {
   return (
     <div>
 
-<div className="footer_container_main">
-          <div className="footer_container">
-            <div className="footer_left_section">
-              <div className="footer_right_section_item">
-                <img src={lg} alt="logo" className="footer_logo" />
-              </div>
+      <div className="footer_container_main">
+        <div className="footer_container">
+          <div className="footer_left_section">
+            <div className="footer_right_section_item">
+              <img src={lg} alt="logo" className="footer_logo" />
             </div>
-            <div className="footer_right_section">
-              <div className="follow_us">
-                <p>Follow us!</p>
-              </div>
-              <div className="footer_right_section_item_social_media">
-                <li>
-                  <a
-                    href="https://www.facebook.com/teamacedigital/"
-                    target="_blank"
-                  >
-                    <img src={facebookicon} alt="facebookicon" />
-                  </a>
-                </li>
+          </div>
+          <div className="footer_right_section">
+            <div className="follow_us">
+              <p>Follow us!</p>
+            </div>
+            <div className="footer_right_section_item_social_media">
+              <li>
+                <a
+                  href="https://www.facebook.com/teamacedigital/"
+                  target="_blank"
+                >
+                  <img src={facebookicon} alt="facebookicon" />
+                </a>
+              </li>
 
-                <li>
-                  <a
-                    href="https://www.instagram.com/teamacedigital/"
-                    target="_blank"
-                  >
-                    <img src={instagram} alt="instagram" className="instagram" />
-                  </a>
-                </li>
-                {/* linkedin */}
-                <li>
-                  <a
-                    href="https://www.linkedin.com/company/ace-digital-solution/"
-                    target="_blank"
-                  >
-                    <img src={linkedin} alt="instagram" />
-                  </a>
-                </li>
-              </div>
-            </div>
-          </div>
-          <div className="footer_heading">
-            <p className="mobile_view">
-              Having clients in over seven sectors and across five continents, Ace
-              digital solution is the fastest growing performance marketing agency
-            </p>
-            <p className="desktop_view">
-              Having clients in over seven sectors and across five continents, Ace
-              digital{" "}
-            </p>
-            <p className="desktop_view">
-              solution is the fastest growing performance marketing agency
-            </p>
-          </div>
-          <div className="portfolio_container">
-            <li>
-              <a
-                href="https://drive.google.com/file/d/1m0tzKzRFSwL9mQghmhu3dPRhEH0IhRTM/view"
-                target="_blank"
-              >
-                {" "}
-                Our Portfolio
-              </a>
-            </li>
-          </div>
-          <div className="footer_bottom_container">
-            <div className="footer_bottom_item">
-              {" "}
-              <img src={googlepartner} alt="googlepartner" />{" "}
-            </div>
-            <div className="footer_bottom_item">
-              <img src={hubspot} alt="googlepartner" />
-            </div>
-            <div className="footer_bottom_item">
-              {" "}
-              <img src={meta} alt="meta" />{" "}
-            </div>
-            <div className="footer_bottom_item">
-              {" "}
-              <img src={aws} alt="aws" />{" "}
+              <li>
+                <a
+                  href="https://www.instagram.com/teamacedigital/"
+                  target="_blank"
+                >
+                  <img src={instagram} alt="instagram" className="instagram" />
+                </a>
+              </li>
+              {/* linkedin */}
+              <li>
+                <a
+                  href="https://www.linkedin.com/company/ace-digital-solution/"
+                  target="_blank"
+                >
+                  <img src={linkedin} alt="instagram" />
+                </a>
+              </li>
             </div>
           </div>
         </div>
+        <div className="footer_heading">
+          <p className="mobile_view">
+            Having clients in over seven sectors and across five continents, Ace
+            digital solution is the fastest growing performance marketing agency
+          </p>
+          <p className="desktop_view">
+            Having clients in over seven sectors and across five continents, Ace
+            digital{" "}
+          </p>
+          <p className="desktop_view">
+            solution is the fastest growing performance marketing agency
+          </p>
+        </div>
+        <div className="portfolio_container">
+          <li>
+            <a
+              href="https://drive.google.com/file/d/1m0tzKzRFSwL9mQghmhu3dPRhEH0IhRTM/view"
+              target="_blank"
+            >
+              {" "}
+              Our Portfolio
+            </a>
+          </li>
+        </div>
+        <div className="footer_bottom_container">
+          <div className="footer_bottom_item">
+            {" "}
+            <img src={googlepartner} alt="googlepartner" />{" "}
+          </div>
+          <div className="footer_bottom_item">
+            <img src={hubspot} alt="googlepartner" />
+          </div>
+          <div className="footer_bottom_item">
+            {" "}
+            <img src={meta} alt="meta" />{" "}
+          </div>
+          <div className="footer_bottom_item">
+            {" "}
+            <img src={aws} alt="aws" />{" "}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
