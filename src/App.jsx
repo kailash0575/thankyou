073d48a83ea84847fbd86a7e3b4db1e414fa9003
Kailash Tanwar <kailash.tanwar@acedigitalsolution.com>
2024-08-14 13:52:14 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from './Footer'
 import React from 'react';
 import Thankyou from './Thankyou'
 import { FloatingWhatsApp } from "react-floating-whatsapp";
-//
+//cvdvc
 import Navbar from './Navbar'
 function App() {
 
@@ -15,7 +15,7 @@ function App() {
 
       {/*  */}
       <div className="main_container">
-        
+
         <FloatingWhatsApp
           accountName="ACE DIGITAL SOLUTION"
           src={null}
@@ -24,10 +24,10 @@ function App() {
           phoneNumber="7070454522"
           avatar="https://www.logo.wine/a/logo/WhatsApp/WhatsApp-Logo.wine.svg"
         />
-            <Navbar/>
-       <Thankyou/>
-       
-         <Footer/>
+        <Navbar />
+        <Thankyou />
+
+        <Footer />
       </div>
     </>
   );
--- src/Thankyou.jsx
+++ src/Thankyou.jsx
@@ -1,17 +1,26 @@
 import Footer from "./Footer"
 import Navbar from "./Navbar"
+import { FloatingWhatsApp } from "react-floating-whatsapp";
 
 const Thankyou = () => {
     return (
         <>
-        {/* <Navbar/> */}
-        <Navbar/>
-        <div className="Thankyou_container">
-            <h1>Thankyou..!</h1>
-            <p>You details has been successfully</p>
-            <p>Submitted. Thanks!</p>
-        </div>
-        <Footer/>
+            <FloatingWhatsApp
+                accountName="ACE DIGITAL SOLUTION"
+                src={null}
+                statusMessage=""
+                chatMessage="How can we help you ACED up? :)"
+                phoneNumber="7070454522"
+                avatar="https://www.logo.wine/a/logo/WhatsApp/WhatsApp-Logo.wine.svg"
+            />
+            {/* <Navbar/> */}
+            <Navbar />
+            <div className="Thankyou_container">
+                <h1>Thankyou..!</h1>
+                <p>You details has been successfully</p>
+                <p>Submitted. Thanks!</p>
+            </div>
+            <Footer />
         </>
     )
 }
